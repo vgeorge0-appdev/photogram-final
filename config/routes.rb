822cc9af77c842fc5521778983c6eab6d2d669a0
vgeorge0 <vgeorge0@chicagobooth.edu>
2021-12-03 17:58:18 +0000

    Photos index looks good

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
+
+
+  get("/photos", { :controller => "photos", :action => "index"})
 end
