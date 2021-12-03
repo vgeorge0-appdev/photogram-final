4450858a2878689c9d48be074e7737739cfeb280
vgeorge0 <vgeorge0@chicagobooth.edu>
2021-12-03 17:37:34 +0000

    User details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index.html.erb"})
   end
+
+  def show
+    # Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+#    if @the_user == nil
+#      redirect_to("/404", status: 404)
+#    else
+    render({ :template => "user_templates/show.html.erb"})
+#  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/users", { :controller => "users", :action => "index"})
+
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
